5e11cac7e70eb2af3313094644ca33c959320ef9
Dan Strandberg <DanStrandberg_@hotmail.com>
2022-01-26 14:47:07 +0100

    enter

--- apibean-app/src/MenuItems.js
+++ apibean-app/src/MenuItems.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import Add from "./assets/graphics/add.svg";
+import React from "react";
 
 function MenuItems() {
-  return <div className="menu-item">
-  <div className="button-class">
-    <button id="add-button"></button>
-  </div>
-    <h1>Kaffe......................49 kr</h1>
-    <p>Bryggd på månadens bönor</p>
-  </div>;
+        
+        function onClick() {
+        alert("hej");
+        }
+  return (
+    <div>
+      <div className="bag">
+      <button id="bag-button" onClick={onClick}></button>
+      </div>
+      <div className="menu-item">
+        <div className="button-class">
+          <button id="add-button" onClick={onClick}></button>
+        </div>
+        <h1>Kaffe......................49 kr</h1>
+        <p>Bryggd på månadens bönor</p>
+      </div>
+    </div>
+  );
 }
 
 export default MenuItems;
