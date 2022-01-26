5e628bbe9e1b0a4e8b837e3d37d45c3e5357728a
Dan Strandberg <DanStrandberg_@hotmail.com>
2022-01-26 12:47:16 +0100

    enter

--- apibean-app/src/Landing.js
+++ apibean-app/src/Landing.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 import rightFlower from "./assets/graphics/intro-graphic-right.svg";
 import leftFlower from "./assets/graphics/intro-graphic-left.svg";
 import loader from "./assets/graphics/airbean-landing.svg";
+import { Link } from "react-router-dom";
 
 function Landing() {
-  return <div className="landing-page">
-    <img id="right-flower" src={rightFlower} alt="right-graphic" />
-    <img id="left-flower" src={leftFlower} alt="left-graphic" />
-    <img id="loader" src={loader} alt="loader-icon" />
-    
-  </div>;
+  return (
+    <div>
+      <Link to="/menu">
+        <div className="landing-page">
+          <img id="right-flower" src={rightFlower} alt="right-graphic" />
+          <img id="left-flower" src={leftFlower} alt="left-graphic" />
+          <img id="loader" src={loader} alt="loader-icon" />
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default Landing;
