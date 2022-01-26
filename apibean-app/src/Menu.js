99df514ef7c921b6dbf2bf71da8dabefe24e56e2
Dan Strandberg <DanStrandberg_@hotmail.com>
2022-01-26 14:19:59 +0100

    enter

--- apibean-app/src/Menu.js
+++ apibean-app/src/Menu.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import topFlower from "./assets/graphics/graphics-header.svg"
-import bottomFlower from "./assets/graphics/graphics-footer.svg"
+import React from "react";
+import topFlower from "./assets/graphics/graphics-header.svg";
+import bottomFlower from "./assets/graphics/graphics-footer.svg";
+import MenuItems from "./MenuItems";
 
 function Menu() {
-  return <div className="menu-page">
-    <img id="top-flower" src={topFlower} alt="top-flower" />
-    <img id="bottom-flower" src={bottomFlower} alt="bottom-flower" />
-    <h1>Meny</h1>
-  </div>;
+  return (
+    <div className="menu-page">
+      <img id="top-flower" src={topFlower} alt="top-flower" />
+      <img id="bottom-flower" src={bottomFlower} alt="bottom-flower" />
+      <h1 id="meny-header">Meny</h1>
+      <div className="menu-layout">
+        <MenuItems />
+      </div>
+    </div>
+  );
 }
 
 export default Menu;
-
---
+++ apibean-app/src/MenuItems.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Add from "./assets/graphics/add.svg";
+
+function MenuItems() {
+  return <div className="menu-item">
+  <div className="button-class">
+    <button id="add-button"></button>
+  </div>
+    <h1>Kaffe......................49 kr</h1>
+    <p>Bryggd på månadens bönor</p>
+  </div>;
+}
+
+export default MenuItems;
