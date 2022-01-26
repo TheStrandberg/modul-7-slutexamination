91fb823cf0d236bdc6d1f2af0cf2975f018ec9cb
Dan Strandberg <DanStrandberg_@hotmail.com>
2022-01-26 12:41:04 +0100

    enter

--- apibean-app/src/Landing.js
+++ apibean-app/src/Landing.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import rightFlower from "./assets/graphics/intro-graphic-right.svg";
+import leftFlower from "./assets/graphics/intro-graphic-left.svg";
+import loader from "./assets/graphics/airbean-landing.svg";
 
 function Landing() {
   return <div className="landing-page">
-
+    <img id="right-flower" src={rightFlower} alt="right-graphic" />
+    <img id="left-flower" src={leftFlower} alt="left-graphic" />
+    <img id="loader" src={loader} alt="loader-icon" />
+    
   </div>;
 }
 
-export default Landing
-;
+export default Landing;
